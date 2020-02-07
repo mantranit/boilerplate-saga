b209137530ba3ca6380e14f71700e1f4b016675b
Man Tran <tranduyminhman@gmail.com>
2020-02-07 14:41:11 +0700

    move privateRoute to utils

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,13 +7,14 @@ import Challenges from 'src/containers/Challenges';
 import Gallery from 'src/containers/Gallery';
 import NotFound from 'src/containers/NotFound';
 
-import PrivateRoute from './PrivateRoute';
+import PrivateRoute from 'src/utils/privateRoute';
 
 export default () => (
 	<Switch>
 		<PrivateRoute exact path="/" component={Home} />
 		<PrivateRoute path="/challenges" component={Challenges} />
 		<PrivateRoute path="/gallery" component={Gallery} />
+
 		<Route path="/signin" component={SignIn} />
 		<Route component={NotFound} />
 	</Switch>
--- src/utils/privateRoute.js
+++ src/utils/privateRoute.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import { Route, Redirect } from 'react-router-dom';
 import AuthStorage from 'src/utils/authStorage';
 
